ffd694bc9bf215880408aaa2bd9d7bde9f3c0c33
Philip Sherman <plsherman@att.net>
2026-08-05 23:20:33 -0400

    Add ping monitor class and shutdown with socket close

--- ObsServer.java
+++ ObsServer.java
@@ -33,9 +33,13 @@
 
 import java.net.*;
 import java.io.*;
+import java.time.*;
+import java.util.concurrent.TimeUnit;
+import java.net.InetAddress;
 
 public class ObsServer
-{
+{private static ServerSocket serverSocket = null;
+	
  public static void main(String[] args) throws IOException
   {System.out.println("OS.main()");
    boolean tracer = true;
@@ -68,9 +72,11 @@ public class ObsServer
    ObsStatus os = new ObsStatus();
    os.setTracer(tracer);
    ObsControl oc = new ObsControl(os,tracer);
+   ObsServerMonitor obsServerMonitor = new ObsServerMonitor(os,tracer);
+   obsServerMonitor.start();
 
    try
-    {ServerSocket serverSocket = new ServerSocket(portNum);
+    {serverSocket = new ServerSocket(portNum);
      serverSocket.setSoTimeout(0);	// never close socket
      while (true)
       {try {new ObsWorkerThread(serverSocket.accept(),oc,os).start();}
@@ -89,6 +95,16 @@ public class ObsServer
 
   }
 
+/*
+ *   this method is only called by the monitor routing as of 2026/08/05 
+*/ 
+ public static void shutdownServer(boolean tracer)
+  {if (tracer) System.out.println("oServer.shutdownServer");
+   try {serverSocket.close();}
+   catch (Exception e) {}			// ignore errors 
+   System.exit(1);
+  }
+
 
  private static void initializeVoltmeter()
   {try
@@ -104,5 +120,85 @@ public class ObsServer
 
    }
 
-  } 
+  }
+
+		 
+  
+ public static class ObsServerMonitor extends Thread
+  {private static LocalTime
+	              timeCurrent
+				 ,timeLast
+				 ;
+   private static ObsServer server;
+   private static ObsStatus os;
+   private static boolean tracer = false
+						 ,pingGood = false
+						 ;
+   private static int testInterval = 15
+					 ;
+	 private static String routerIPAddress ="192.168.65.1";
+	 private static String[] IPAddresses = {"192.168.0.1","192.168.65.1"}; // observatory & Phil's house
+	 private static InetAddress routerInetAddress = null;
+
+   public ObsServerMonitor(ObsStatus oStatus, boolean b1)
+    {// server = oServer;
+	 os = oStatus;
+	 tracer = b1;
+	} 
+
+   public void run()
+    {if (tracer) System.out.println("OSM.run()");
+	 timeLast = LocalTime.now();
+	 int reportInterval = 4*60;  // (4hrs * 60min) report every 4 hours
+	 
+// *******************************  start test only code  ***********************
+//	 reportInterval = 2;
+//	 testInterval   = 1;
+// *******************************  end   test only code  ***********************
+int killCount = 5;
+	 while (true)
+	  {
+	   try {TimeUnit.MINUTES.sleep(testInterval);}
+	   catch(InterruptedException e) {}
+	   timeCurrent = LocalTime.now();
+	   if (timeCurrent.minusMinutes(reportInterval).isAfter(timeLast))
+	    {if (tracer) System.out.println("OSM.run() all is well at "+timeCurrent);
+		 timeLast = timeCurrent;
+	    }
+	   if (pingTest())
+	     continue;
+/*
+ *
+ * Ping failed - wait 3 minutes then try again. 2nd failure force restart by exiting 
+*/
+       try {TimeUnit.MINUTES.sleep((long)3);}
+	   catch(InterruptedException e) {} 
+	   if (!pingTest())
+	    {System.out.println("two 3 min apart pings failed - terminating ObsServer");
+		 shutdownServer(true);
+	    }    
+      } 
+    } 
+  private static boolean pingTest()
+   {if (tracer) System.out.println("OSM.pingTest()");
+	pingGood = false;
+	try
+	 {for (String routerIPAddress : IPAddresses)
+       {routerInetAddress = InetAddress.getByName(routerIPAddress);
+		if (tracer) System.out.println("  testing ["+routerIPAddress+"]");
+		if (routerInetAddress.isReachable(125))		// response s/b < 1/8 second
+	     {pingGood = true;
+		  break;
+	     }
+	   }  
+     }
+    catch (IOException e)
+     {System.out.println("   ping IOException\n   "+e);
+	  System.exit(1);
+	 }
+	return pingGood;
+   }
+  }
+  
+			
 }
